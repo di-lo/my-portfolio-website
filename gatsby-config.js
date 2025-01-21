3dfca44aaa7603451f51b592c2aeb6e673a8e6d6
Dilara <60364755+di-lo@users.noreply.github.com>
2025-01-21 14:23:15 -0600

    Add files via upload

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `My Portfolio Website`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
